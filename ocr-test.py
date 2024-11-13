24b88183876f62c717364a5f27b1dd2899277e22
SeanChen2 <seanchen.sc2@gmail.com>
2024-11-12 20:05:26 -0500

    Initial Python Commit

---
+++ ocr-test.py
@@ -0,0 +1,5 @@
+# Plan:
+# - Use NIST Special Database for digits and characters
+# - MNIST for digits 0-9: https://yann.lecun.com/exdb/mnist/
+# - Need to get the rest of the characters. May use this for A-Z as a starting point: https://www.kaggle.com/datasets/sachinpatel21/az-handwritten-alphabets-in-csv-format
+# https://github.com/frereit/TensorflowHandwritingRecognition/blob/master/Preprocessing%20the%20data.md - extracting from NIST manually
